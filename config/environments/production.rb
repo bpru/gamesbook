da9dadb49150d88af312ace855be05ffd0960951
bpru <bpru7443@gmail.com>
2015-09-04 17:05:33 +0000

    change heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-lake-7584.herokuapp.com'
+  # host = 'quiet-lake-7584.herokuapp.com'
+  host = 'gamesbook.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
