914dac6425844676b6db09d25498e8463829b424
bpru <bpru7443@gmail.com>
2015-09-20 01:25:28 +0000

    fix bugs for previous page link after logging in from home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @return_url = nil
-      if show_previous? and session[:previous_page] != "https://gamesbook-bpru.c9.io/"
+      if show_previous? and session[:previous_page] != root_url and session[:previous_page] != login_url
         @return_url = session[:previous_page]
         session[:previous_page] = nil
       end
